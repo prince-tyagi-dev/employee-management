a1b866899e4d15c79af9f444178116bf7acb40aa
Prince Tyagi <98056346+prince-tyagi-dev@users.noreply.github.com>
2023-05-27 17:38:38 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./contexts/AuthProvider";
 
